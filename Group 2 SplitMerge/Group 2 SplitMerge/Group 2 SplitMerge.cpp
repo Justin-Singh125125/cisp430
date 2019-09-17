702418ef00ef9f1d518aaf7fdaa5b70cceb78b58
justin singh <justin.singh125125@gmail.com>
2019-09-17 12:18:36 -0700

    fixed separateNode function

--- Group 2 SplitMerge/Group 2 SplitMerge/Group 2 SplitMerge.cpp
+++ Group 2 SplitMerge/Group 2 SplitMerge/Group 2 SplitMerge.cpp
@@ -51,15 +51,15 @@ Node* ReadFile() {
 		//create a new node for each name
 		Node* newNode = new Node;
 
-		//read current name
-		read >> newNode->name;
-		newNode->next = NULL;
+//read current name
+read >> newNode->name;
+newNode->next = NULL;
 
-		//link new node to the head
-		currentNode->next = newNode;
+//link new node to the head
+currentNode->next = newNode;
 
-		//move current node to the next linked node
-		currentNode = currentNode->next;
+//move current node to the next linked node
+currentNode = currentNode->next;
 	}
 
 	//close the file
@@ -99,22 +99,112 @@ void SplitAndMerge(Node* head) {
 	Node* list_2 = new Node;
 
 	//flag variables
-	bool isFirst_1 = true;
-	bool isFirst_2 = true;
+	bool isFirst = true;
 
 	//keep track of current node
 	Node* currentNode = head;
 	Node* currentNewNode = list_1;
-		
+
+	//create first list
+	for (int i = 1; i <= (totalNodes / 2); i++) {
+
+		if (isFirst) {
+			//get first name
+			list_1->name = currentNode->name;
+
+			//set node to null
+			list_1->next = NULL;
+
+			//make current head traverse
+			currentNode = currentNode->next;
+
+			//set flag variable to false
+			isFirst = false;
+
+		}
+		else {
+
+			//before we create a new node, lets check to see 
+			//if there is even another node to append first
+			if (totalNodes != 2) {
+				Node* newNode = new Node;
+
+
+				//set new node to whatever current node is pointing to
+				newNode->name = currentNode->name;
+				//set newNode next to null
+				newNode->next = NULL;
+
+				//link newNode
+				currentNewNode->next = newNode;
+
+				//move the currentNewNode to the appended new node
+				currentNewNode = currentNewNode->next;
+
+				//if currentNode->next does not have null
+				if (currentNode->next) {
+					currentNode = currentNode->next;
+				}
+			}
+
+		}
+	}
+
+	//generate second list
+	isFirst = true;
+	currentNewNode = list_2;
+
+	for (int i = (totalNodes / 2) + 1; i <= totalNodes; i++) {
+		if (isFirst) {
+			//get first name
+			list_2->name = currentNode->name;
+
+			//set node to null
+			list_2->next = NULL;
+
+			//make current head traverse
+			currentNode = currentNode->next;
+
+			//set flag variable to false
+			isFirst = false;
+
+		}
+		else {
+
+			if (totalNodes != 2) {
+				Node* newNode = new Node;
+
+
+				//set new node to whatever current node is pointing to
+				newNode->name = currentNode->name;
+				//set newNode next to null
+				newNode->next = NULL;
+
+				//link newNode
+				currentNewNode->next = newNode;
+
+				//move the currentNewNode to the appended new node
+				currentNewNode = currentNewNode->next;
+
+				//if currentNode->next does not have null
+				if (currentNode->next) {
+					currentNode = currentNode->next;
+				}
+			}
+		}
+	}
+
+
+
 	cout << "list 1: " << endl;
-	//Traverse(list_1);
+	Traverse(list_1);
 
 	cout << "list 2: " << endl;
 
-	//Traverse(list_2);
-
-
+	Traverse(list_2);
 }
+
+
 
 
 
