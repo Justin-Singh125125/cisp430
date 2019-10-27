75b3e456e063f6ad1f8914a4921847c085c27d4c
Justin Singh <justin.singh125125@gmail.com>
2019-10-27 00:22:12 -0700

    working on quick sort

--- QuickSortRadixSort/QuickSortRadixSort/main.cpp
+++ QuickSortRadixSort/QuickSortRadixSort/main.cpp
@@ -8,21 +8,21 @@
 
 using namespace std;
 
-
 struct Node
 {
-	Node* next;
+	Node *next;
 	int data;
 };
 
 struct Queue
 {
-	Node* front;
-	Node* rear;
+	Node *front;
+	Node *rear;
 };
 
+const int SIZE = 10;
 
-void enqueue(Queue& q, int data)
+void enqueue(Queue &q, int data)
 {
 
 	if (!q.front)
@@ -42,8 +42,7 @@ void enqueue(Queue& q, int data)
 	}
 }
 
-
-int dequeue(Queue& q)
+int dequeue(Queue &q)
 {
 
 	if (!q.front)
@@ -51,7 +50,7 @@ int dequeue(Queue& q)
 		return NULL;
 	}
 	int value = 0;
-	Node* temp = q.front;
+	Node *temp = q.front;
 	if (q.front == q.rear)
 	{
 		q.front = q.rear = NULL;
@@ -66,9 +65,10 @@ int dequeue(Queue& q)
 	return value;
 }
 
-
-void convertInt(int* arr, string* stringArr, int arrSize) {
-	for (int i = 0; i < arrSize; i++) {
+void convertInt(int *arr, string *stringArr, int arrSize)
+{
+	for (int i = 0; i < arrSize; i++)
+	{
 		arr[i] = stoi(stringArr[i]);
 	}
 }
@@ -82,23 +82,18 @@ int getMax(int *arr, int SIZE)
 	return mx;
 }
 
-
-
-
-
-
-void radixSortDecending(Queue* q, int *arr, int SIZE)
+void radixSortDecending(Queue *q, int arr[], int arrSize)
 {
 
 	//find the maximum number to know the number of digits we going to go by
-	int m = getMax(arr, SIZE);
+	int m = getMax(arr, arrSize);
 
 	//number of passes
 	for (int n = 1; n <= m; n *= 10)
 	{
 
 		//number of items in array
-		for (int i = 0; i < SIZE; i++)
+		for (int i = 0; i < arrSize; i++)
 		{
 
 			int queueIndex = arr[i] % (10 * n);
@@ -110,7 +105,7 @@ void radixSortDecending(Queue* q, int *arr, int SIZE)
 		//now we need to dequeue and put the items into the array
 
 		int count = 0;
-		for (int i = SIZE - 1; i >= 0; i--)
+		for (int i = arrSize - 1; i >= 0; i--)
 		{
 
 			while (q[i].front != NULL)
@@ -122,15 +117,16 @@ void radixSortDecending(Queue* q, int *arr, int SIZE)
 	}
 }
 
-void convertString(int* arr, string* stringArr, int arrSize) {
-	for (int i = 0; i < arrSize; i++) {
+void convertString(int *arr, string *stringArr, int arrSize)
+{
+	for (int i = 0; i < arrSize; i++)
+	{
 		stringArr[i] = to_string(arr[i]);
 	}
-
 }
 
-// A utility function to swap two elements  
-void swap(int* a, int* b)
+// A utility function to swap two elements
+void swap(int *a, int *b)
 {
 	int t = *a;
 	*a = *b;
@@ -139,15 +135,15 @@ void swap(int* a, int* b)
 
 int partition(int arr[], int low, int high)
 {
-	int pivot = arr[high]; // pivot  
-	int i = (low - 1); // Index of smaller element  
+	int pivot = arr[high]; // pivot
+	int i = (low - 1);	 // Index of smaller element
 
 	for (int j = low; j <= high - 1; j++)
 	{
-		// If current element is smaller than the pivot  
+		// If current element is smaller than the pivot
 		if (arr[j] < pivot)
 		{
-			i++; // increment index of smaller element  
+			i++; // increment index of smaller element
 			swap(&arr[i], &arr[j]);
 		}
 	}
@@ -163,100 +159,100 @@ void quickSort(int *arr, int low, int high)
 		at right place */
 		int pi = partition(arr, low, high);
 
-		// Separately sort elements before  
-		// partition and after partition  
+		// Separately sort elements before
+		// partition and after partition
 		quickSort(arr, low, pi - 1);
 		quickSort(arr, pi + 1, high);
 	}
 }
 
-void convertToArray(vector<string> vec, int arr []) {
+void convertToArray(vector<string> vec, int arr[])
+{
 	int count = 0;
-	for (std::vector<string>::const_iterator i = vec.begin(); i != vec.end(); ++i) {
+	for (std::vector<string>::const_iterator i = vec.begin(); i != vec.end(); ++i)
+	{
 		arr[count] = stoi(*i);
 		count++;
 	}
 }
 
-
 template <class Container>
-void split(const std::string& str, Container& cont, char delim = ' ')
+void split(const std::string &str, Container &cont, char delim = ' ')
 {
 	std::stringstream ss(str);
 	std::string token;
-	while (std::getline(ss, token, delim)) {
+	while (std::getline(ss, token, delim))
+	{
 		cont.push_back(token);
 	}
 }
 
-void readFile(vector<string> &vec) {
-	
+void readFile(vector<string> &vec)
+{
+
 	//read from text file
 	ifstream read("input.txt");
-	
+
 	//read the text
 	string text;
 
 	getline(read, text);
 
 	split(text, vec, ' ');
-
-	
 }
 int main()
 {
 	//create a vector
-	 vector<string> vec;
-	
+	vector<string> vec;
+
 	//cread from file
 	readFile(vec);
 
 	//dynamically allocate an array
 	int arrSize = vec.size();
-	int* arr = new int[arrSize];
+	int *arr = new int[arrSize];
 
 	convertToArray(vec, arr);
 
 	//quick sort from last pivot
-	quickSort(arr,0, arrSize-1);
+	quickSort(arr, 0, arrSize - 1);
 
 	cout << "AFTER QUICK SORT" << endl;
-	for (int i = 0; i < arrSize; i++) {
+	for (int i = 0; i < arrSize; i++)
+	{
 		cout << arr[i] << endl;
 	}
 
-	
 	//convert array to string
-	string *stringArr = new string [arrSize];
+	string *stringArr = new string[arrSize];
 
 	convertString(arr, stringArr, arrSize);
 
 	int m = getMax(arr, arrSize);
 
-
 	//get num of characters to have
 	string temp = to_string(m);
 	int numCount = temp.size();
 
-	
-	for (int i = 0; i < arrSize; i++) {
-		
-		if (stringArr[i].size() < numCount) {
+	for (int i = 0; i < arrSize; i++)
+	{
+
+		if ((stringArr[i].size()) < numCount)
+		{
 
 			int count = stringArr[i].size();
-			for (int j = 0; j < (numCount - count); j++) {
+			for (int j = 0; j < (numCount - count); j++)
+			{
 				stringArr[i] += "5";
 			}
 		}
 	}
 
-
 	convertInt(arr, stringArr, arrSize);
 
+	delete[] stringArr;
 
-
-
-	Queue *q = new Queue[arrSize];
+	Queue q[SIZE];
 
 	//initalize all queues and stacks to NULL
 	for (int i = 0; i < 10; i++)
@@ -264,17 +260,15 @@ int main()
 		q[i].front = q[i].rear = NULL;
 	}
 
-
 	radixSortDecending(q, arr, arrSize);
 
-
 	cout << "AFTER RADIX SORT" << endl;
-	for (int i = 0; i < arrSize; i++)
+	for (int i = arrSize - 1; i >= 0; i--)
 	{
-		cout << arr[i] << " ";
+		cout << arr[i] << endl;
 	}
 	cout << endl;
 
+	delete[] arr;
 	return 0;
 }
-
