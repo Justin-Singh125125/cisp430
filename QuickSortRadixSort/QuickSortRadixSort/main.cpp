e1adc91d5dd7d53ca41337ff5dd19b0ad8517f5f
Justin Singh <justin.singh125125@gmail.com>
2019-10-26 23:16:01 -0700

    working on quick sort

--- QuickSortRadixSort/QuickSortRadixSort/main.cpp
+++ QuickSortRadixSort/QuickSortRadixSort/main.cpp
@@ -8,19 +8,118 @@
 
 using namespace std;
 
+
+struct Node
+{
+	Node* next;
+	int data;
+};
+
+struct Queue
+{
+	Node* front;
+	Node* rear;
+};
+
+
+void enqueue(Queue& q, int data)
+{
+
+	if (!q.front)
+	{
+		q.front = new Node;
+		q.rear = q.front;
+		q.front->data = data;
+		q.front->next = NULL;
+	}
+	else
+	{
+		//we are going to be adding at the rear everytime for queue
+		q.rear->next = new Node;
+		q.rear = q.rear->next;
+		q.rear->data = data;
+		q.rear->next = NULL;
+	}
+}
+
+
+int dequeue(Queue& q)
+{
+
+	if (!q.front)
+	{
+		return NULL;
+	}
+	int value = 0;
+	Node* temp = q.front;
+	if (q.front == q.rear)
+	{
+		q.front = q.rear = NULL;
+		value = temp->data;
+	}
+	else
+	{
+		q.front = q.front->next;
+		value = temp->data;
+	}
+	delete temp;
+	return value;
+}
+
+
 void convertInt(int* arr, string* stringArr, int arrSize) {
 	for (int i = 0; i < arrSize; i++) {
 		arr[i] = stoi(stringArr[i]);
 	}
 }
 
-int getMax(int arr[], int SIZE)
+int getMax(int *arr, int SIZE)
 {
 	int mx = arr[0];
 	for (int i = 1; i < SIZE; i++)
 		if (arr[i] > mx)
 			mx = arr[i];
 	return mx;
+}
+
+
+
+
+
+
+void radixSortDecending(Queue* q, int *arr, int SIZE)
+{
+
+	//find the maximum number to know the number of digits we going to go by
+	int m = getMax(arr, SIZE);
+
+	//number of passes
+	for (int n = 1; n <= m; n *= 10)
+	{
+
+		//number of items in array
+		for (int i = 0; i < SIZE; i++)
+		{
+
+			int queueIndex = arr[i] % (10 * n);
+			queueIndex /= n;
+
+			enqueue(q[queueIndex], arr[i]);
+		}
+
+		//now we need to dequeue and put the items into the array
+
+		int count = 0;
+		for (int i = SIZE - 1; i >= 0; i--)
+		{
+
+			while (q[i].front != NULL)
+			{
+				arr[count] = dequeue(q[i]);
+				count++;
+			}
+		}
+	}
 }
 
 void convertString(int* arr, string* stringArr, int arrSize) {
@@ -121,7 +220,10 @@ int main()
 	//quick sort from last pivot
 	quickSort(arr,0, arrSize-1);
 
-
+	cout << "AFTER QUICK SORT" << endl;
+	for (int i = 0; i < arrSize; i++) {
+		cout << arr[i] << endl;
+	}
 
 	
 	//convert array to string
@@ -151,9 +253,27 @@ int main()
 
 	convertInt(arr, stringArr, arrSize);
 
-	for (int i = 0; i < arrSize; i++) {
-		cout << arr[i] << endl;
+
+
+
+	Queue *q = new Queue[arrSize];
+
+	//initalize all queues and stacks to NULL
+	for (int i = 0; i < 10; i++)
+	{
+		q[i].front = q[i].rear = NULL;
 	}
+
+
+	radixSortDecending(q, arr, arrSize);
+
+
+	cout << "AFTER RADIX SORT" << endl;
+	for (int i = 0; i < arrSize; i++)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << endl;
 
 	return 0;
 }
